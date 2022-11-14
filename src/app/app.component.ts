4b4bffd2be377405e804814ce3e9e84e05dc6b12
Христюк Ксения Алексеевна <@itstep.loc>
2022-11-14 16:44:15 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-exam';
+export class AppComponent implements OnInit{
+  // name: string = "alex" 
+  names:string[] = ['alex', 'bob', 'carl'] 
+  nameTmp: string = "" 
+  ngOnInit(): void { 
+  } 
+ 
+  addName() { 
+    this.names.push(this.nameTmp) 
+    this.nameTmp="" 
+  } 
+  deleteName(idx:number){ 
+    this.names.splice(idx,1) 
+} 
 }
+
